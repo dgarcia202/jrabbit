4165691d9594e07c2bc7da88538d38827def9389
dgarcia202 <wft2000@gmail.com>
2017-04-09 13:41:02 +0200

    improve readability of run cmd

--- run.cmd
+++ run.cmd
@@ -1,2 +1,8 @@
 @echo off
-java -cp target\classes\;\Users\Diego\.m2\repository\com\rabbitmq\amqp-client\4.1.0\amqp-client-4.1.0.jar;\Users\Diego\.m2\repository\org\slf4j\slf4j-api\1.7.21\slf4j-api-1.7.21.jar;\Users\Diego\.m2\repository\org\slf4j\slf4j-nop\1.7.21\slf4j-nop-1.7.21.jar com.github.dgarcia202.App
+
+java -cp ^
+target\classes\;^
+\Users\Diego\.m2\repository\com\rabbitmq\amqp-client\4.1.0\amqp-client-4.1.0.jar;^
+\Users\Diego\.m2\repository\org\slf4j\slf4j-api\1.7.21\slf4j-api-1.7.21.jar;^
+\Users\Diego\.m2\repository\org\slf4j\slf4j-nop\1.7.21\slf4j-nop-1.7.21.jar ^
+com.github.dgarcia202.App
