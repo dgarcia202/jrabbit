ecf17ed3c6c7a77a3ae4b9110b5c33bacb4f6ea8
dgarcia202 <wft2000@gmail.com>
2017-04-11 22:19:51 +0200

    add jackson package as a dependency

--- run.cmd
+++ run.cmd
@@ -4,5 +4,6 @@ java -cp ^
 target\classes\;^
 %HOME%\.m2\repository\com\rabbitmq\amqp-client\4.1.0\amqp-client-4.1.0.jar;^
 %HOME%\.m2\repository\org\slf4j\slf4j-api\1.7.21\slf4j-api-1.7.21.jar;^
-%HOME%\.m2\repository\org\slf4j\slf4j-nop\1.7.21\slf4j-nop-1.7.21.jar ^
+%HOME%\.m2\repository\org\slf4j\slf4j-nop\1.7.21\slf4j-nop-1.7.21.jar;^
+%HOME%.m2\repository\com\fasterxml\jackson\core\jackson-core\2.6.5\jackson-core-2.6.5.jar^
 com.github.dgarcia202.App
